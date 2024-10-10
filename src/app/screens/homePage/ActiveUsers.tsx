1be7a24cf61a1c2b0cb74a52daecbf2f8860e488
Abrorkhon <akon.xan@mail.ru>
2024-10-10 19:21:38 +0900

    feat Events Sectional Components

--- src/app/screens/homePage/ActiveUsers.tsx
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -1,10 +1,55 @@
-import React from "react";
-import { Container } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
+import Card from "@mui/joy/Card";
+import { CssVarsProvider, Typography } from "@mui/joy";
+import CardOverflow from "@mui/joy/CardOverflow";
+import AspectRatio from "@mui/joy/AspectRatio";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import Divider from "../../components/divider";
+
+const activeUsers = [
+  { productName: "Martin", imagePath: "/img/martin.webp" },
+  { productName: "Justin", imagePath: "/img/justin.webp" },
+  { productName: "Rose", imagePath: "/img/rose.webp" },
+  { productName: "Nusret", imagePath: "/img/nusret.webp" },
+];
+
 export default function ActiveUsers() {
   return (
-    <div>
-      {" "}
-      <Container> ActiveUsers</Container>
+    <div className={"active-users-frame"}>
+      <Container>
+        <Stack className={"main"}>
+          <Box className={"category-title"}>Fresh Menu</Box>
+          <Stack className={"cards-frame"}>
+            <CssVarsProvider>
+              {activeUsers.length !== 0 ? (
+                activeUsers.map((ele, index) => {
+                  return (
+                    <Card key={index} variant="outlined" className={"card"}>
+                      <CardOverflow>
+                        <AspectRatio ratio="1">
+                          <img src={ele.imagePath} alt="" />
+                        </AspectRatio>
+                      </CardOverflow>
+
+                      <CardOverflow variant="soft" className="product-detail">
+                        <Stack className="info">
+                          <Stack flexDirection={"row"}>
+                            <Typography className={"member-nickname"}>
+                              {ele.productName}
+                            </Typography>
+                          </Stack>
+                        </Stack>
+                      </CardOverflow>
+                    </Card>
+                  );
+                })
+              ) : (
+                <Box className="no-data"> Active Users are not available</Box>
+              )}
+            </CssVarsProvider>
+          </Stack>
+        </Stack>
+      </Container>
     </div>
   );
 }
--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Statistics from "./Statistics";
-
-
 import NewDishes from "./NewDishes";
 import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
 import PopularDishes from "./PopularDishes";
+import "../../../css/home.css"
 
 export default function HomePage() {
   return (
